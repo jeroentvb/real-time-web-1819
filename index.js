d5b1d1fe2eb9a96894b88e860c1158ffcff08ed0
jeroentvb <jeroen.van.berkum@hva.nl>
2019-04-08 17:52:39 +0200

    Really basic but working socket

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')
+const http = require('http').Server(app)
 
-app()
+const server = app()
   .set('view engine', 'ejs')
   .set('views', 'templates/pages')
   .use(app.static('static'))
@@ -13,9 +14,15 @@ app()
   .use(notFound)
   .listen(process.env.PORT || 3000, () => console.log(`[server] listening on port ${process.env.PORT || 3000}`))
 
+const io = require('socket.io')(server)
+
 function index (req, res) {
   res.render('index')
 }
+
+io.on('connection', socket => {
+  console.log('a user connected')
+})
 
 function notFound (req, res) {
   res.status(404).render('error', {
