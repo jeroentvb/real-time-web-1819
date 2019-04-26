8c57189d4ae5c5d6dae477d242834b9bf865c0f6
jeroentvb <jeroentvb@hotmail.com>
2019-04-26 12:22:49 +0200

    Cleaned the repo - fixes #1

--- pokemon-battle/README.md
+++
@@ -1,2 +0,0 @@
-# pokemon
-Multiplayer pokemon fight
--- pokemon-battle/partials/helper.js
+++
@@ -1,7 +0,0 @@
-function attackDamage (damage) {
-  return parseInt(damage) + (Math.floor(Math.random() * 10) + 1)
-}
-
-module.exports = {
-  attackDamage
-}
